c12a34624f354839cd40c7c9249f914a361992db
mfasahin <sahinmehmet01q@gmail.com>
2026-07-10 14:47:48 +0300

    added new models (ihtar, icra)

---
+++ Presentation/Models/IcraMahkemeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Presentation.Models
+{
+    public class IcraMahkemeModel
+    {
+        public int MAHKEME_ID { get; set; } // PK
+        
+        public string MAHKEME_AD { get; set; } // NOT NULL
+    }
+}
---
+++ Presentation/Models/IhtarUrunModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Presentation.Models
+{
+    public class IhtarUrunModel
+    {
+        public int IHTAR_URUN_ID { get; set; } // PK
+
+        public int URUN_ID { get; set; } // FK
+
+        public int IHTAR_ID { get; set; } // FK
+    }
+}
